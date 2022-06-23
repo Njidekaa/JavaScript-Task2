c9487409528cdc2ef6df119bedba313478f9ccde
unknown <onyishiprecious73@gmail.com>
2022-06-23 23:57:00 +0100

    Simple Calculator designed with Javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+const x = prompt("Enter first number");
+const y = prompt("Enter second number");
+
+const operation = prompt("Select the operator of choice: (+, -, *, /)");
+
+
+if (operation == "+") {
+    result = parseFloat(x) + parseFloat(y);
+} else if (operation == "-") {
+    result = parseFloat(x) - parseFloat(y);
+} else if (operation == "*") {
+    result = parseFloat(x) * parseFloat(y);;
+} else if (operation == "/") {
+    result = parseFloat(x) / parseFloat(y);
+}
+
+else{
+    alert("Invalid Operation, Please Try Again.");
+}
+
+alert("The result is : " + result);
